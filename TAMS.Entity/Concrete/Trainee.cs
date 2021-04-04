4a89b2227c7719a45948f9e5e53c2a2cc7a1b0ee
Bekir Can BAYKAL <bcbsoftware@hotmail.com>
2021-04-05 02:32:50 +0300

    Trainee.cs entity has been added. Database name has been changed into TennisAcademyManagementSystemDB in appsettings.json

---
+++ TAMS.Entity/Concrete/Trainee.cs
@@ -0,0 +1,12 @@
+﻿using TAMS.Entity.Base;
+
+namespace TAMS.Entity.Concrete
+{
+    public class Trainee : EntityBase
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
